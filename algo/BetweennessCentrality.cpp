95e167a0ed2870c98a1f601f2934ff890e94a030
Josh Levy <jdlevy3@illinois.edu>
2020-12-07 17:28:45 -0600

    Added betweenness centrality

---
+++ algo/BetweennessCentrality.cpp
@@ -0,0 +1,37 @@
+#include "BetweennessCentrality.h"
+
+std::vector<Vertex> findCentralAirports(int numAirports, Graph G) {
+    std::map<Vertex, int> vertexCentrality;
+    std::vector<Vertex> vertices;
+    int pathCount = 0;
+    BFS::BFS BFS;
+    BFS.addGraph(G);
+    for (size_t i = 0; i < vertices.size(); i++) {
+        for (size_t j = 0; j < vertices.size(); j++) {
+            if (i != j) {
+                std::vector<Vertex> shortestPath = BFS.findShortestPath(vertices[i], vertices[j]); //Returns vector of nodes in shortest path between 2 given nodes, not including the given start and end. Empty if no path.
+                if (shortestPath.size() != 0) {
+                    pathCount++;
+                }
+                for (const Vertex &v : shortestPath) {
+                    vertexCentrality[v]++;
+                }
+            }
+        }
+    }
+    //Remove top numAirports vertices.
+    std::vector<std::pair<int, Vertex>> centrality; 
+    for(auto const& p : vertexCentrality) {
+        centrality.push_back(make_pair(p.second, p.first));
+    }
+
+    std::sort(centrality.begin(), centrality.end());
+    std::reverse(centrality.begin(), centrality.end());
+
+    std::vector<Vertex> centralAirports;
+    for (size_t i = 0; i < numAirports; i++) {
+        centralAirports.push_back(centrality[i].second);
+    }
+    //centralAirports is now a vector with the top airports to be removed
+    return centralAirports;
+}
---
+++ algo/BetweennessCentrality.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <queue>
+#include <algorithm>
+
+#include "../graph/graph.h"
+#include "BFS.h"
+
+std::vector<Vertex> findCentralAirports(int numAirports, Graph G);
