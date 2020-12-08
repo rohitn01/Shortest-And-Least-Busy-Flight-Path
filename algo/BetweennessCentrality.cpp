371e85238e49418a427b12e9c1f520cf8a3521b5
Josh Levy <jdlevy3@illinois.edu>
2020-12-07 18:07:07 -0600

    Added betweenness centrality comments

--- algo/BetweennessCentrality.cpp
+++ algo/BetweennessCentrality.cpp
@@ -4,12 +4,11 @@ std::vector<Vertex> findCentralAirports(int numAirports, Graph G) {
     std::map<Vertex, int> vertexCentrality;
     std::vector<Vertex> vertices;
     int pathCount = 0;
-    BFS::BFS BFS;
-    BFS.addGraph(G);
+    Dijkstra::Dijkstra Dijkstra(G);
     for (size_t i = 0; i < vertices.size(); i++) {
         for (size_t j = 0; j < vertices.size(); j++) {
             if (i != j) {
-                std::vector<Vertex> shortestPath = BFS.findShortestPath(vertices[i], vertices[j]); //Returns vector of nodes in shortest path between 2 given nodes, not including the given start and end. Empty if no path.
+                std::vector<Vertex> shortestPath = Dijkstra.findShortestPath(vertices[i], vertices[j]); //Returns vector of nodes in shortest path between 2 given nodes, not including the given start and end. Empty if no path.
                 if (shortestPath.size() != 0) {
                     pathCount++;
                 }
@@ -19,7 +18,8 @@ std::vector<Vertex> findCentralAirports(int numAirports, Graph G) {
             }
         }
     }
-    //Remove top numAirports vertices.
+
+    //Find top numAirports vertices.
     std::vector<std::pair<int, Vertex>> centrality; 
     for(auto const& p : vertexCentrality) {
         centrality.push_back(make_pair(p.second, p.first));
@@ -32,6 +32,6 @@ std::vector<Vertex> findCentralAirports(int numAirports, Graph G) {
     for (size_t i = 0; i < numAirports; i++) {
         centralAirports.push_back(centrality[i].second);
     }
-    //centralAirports is now a vector with the top airports to be removed
+    //centralAirports is now a vector with the top airports
     return centralAirports;
 }
--- algo/BetweennessCentrality.h
+++ algo/BetweennessCentrality.h
@@ -7,5 +7,15 @@
 
 #include "../graph/graph.h"
 #include "BFS.h"
+#include "Dijkstra.h"
 
+/**
+ * Finds the most central airports in the provided graph using the unweighted version of the betweenness centrality algorithm.
+ * The unweighted version is used since the number of flights at an airports is more relevant than the distices of those flights.
+ * 
+ * @param numAirports The number of central airports to find
+ * @param G Graph to search
+ * 
+ * @return A vector with the most central vertices (airports)
+ */
 std::vector<Vertex> findCentralAirports(int numAirports, Graph G);
