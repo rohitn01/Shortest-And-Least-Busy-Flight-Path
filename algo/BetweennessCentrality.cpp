9252c4a746199f97486b7f4a2a500f69108d0367
Josh Levy <jdlevy3@illinois.edu>
2020-12-08 20:59:47 -0600

    modified betweenness centrality

--- algo/BetweennessCentrality.cpp
+++ algo/BetweennessCentrality.cpp
@@ -1,28 +1,35 @@
 #include "BetweennessCentrality.h"
+#include <iostream>
 
 std::vector<Vertex> findCentralAirports(int numAirports, Graph& G) {
-    std::map<Vertex, int> vertexCentrality;
+    std::unordered_map<Vertex, int> vertexCentrality;
     std::vector<Vertex> vertices = G.getVertices();
-    int pathCount = 0;
+    if (numAirports >= vertices.size()) {
+        return vertices;
+    }
+    if (numAirports <= 0) {
+        return std::vector<Vertex>();
+    } 
     Dijkstra Dijkstra(G);
     for (size_t i = 0; i < vertices.size(); i++) {
         for (size_t j = 0; j < vertices.size(); j++) {
             if (i != j) {
+                std::cout<<"checking: "<<vertices[i]<<" to "<<vertices[j]<<std::endl;
                 std::vector<Vertex> shortestPath = Dijkstra.findShortestPath(vertices[i], vertices[j]); //Returns vector of nodes in shortest path between 2 given nodes, not including the given start and end. Empty if no path.
-                if (shortestPath.size() != 0) {
-                    pathCount++;
-                }
+                std::cout<<"Path found"<<std::endl;
+
                 for (const Vertex &v : shortestPath) {
                     vertexCentrality[v]++;
                 }
+                std::cout<<"centrality appened"<<std::endl;
             }
         }
     }
 
     //Find top numAirports vertices.
     std::vector<std::pair<int, Vertex>> centrality; 
-    for(auto const& p : vertexCentrality) {
-        centrality.push_back(make_pair(p.second, p.first));
+    for(Vertex v : vertices) {
+        centrality.push_back(make_pair(vertexCentrality[v], v));
     }
 
     std::sort(centrality.begin(), centrality.end());
@@ -31,6 +38,9 @@ std::vector<Vertex> findCentralAirports(int numAirports, Graph& G) {
     std::vector<Vertex> centralAirports;
     for (int i = 0; i < numAirports; i++) {
         centralAirports.push_back(centrality[i].second);
+    }
+    for (auto &v : centrality) {
+        std::cout<<v.second<<": "<<v.first<<std::endl;
     }
     //centralAirports is now a vector with the top airports
     return centralAirports;
--- main.cpp
+++ main.cpp
@@ -23,18 +23,18 @@ int main() {
   test.insertVertex("A");
   test.insertVertex("B");
   test.insertVertex("C");
-  //test.insertVertex("D");
-  //test.insertVertex("E");
-  //test.insertVertex("F");
-  test.insertEdge("A", "B", 9);
-  test.insertEdge("B", "C", 8);
-  //test.insertEdge("A", "D", 4);
-  //test.insertEdge("C", "D", 5);
-  //test.insertEdge("D", "E", 7);
-  //test.insertEdge("D", "F", 2);
+  test.insertVertex("D");
+  test.insertVertex("E");
+  test.insertVertex("F");
+  test.insertEdge("A", "C", 2);
+  test.insertEdge("D", "C", 2);
+  test.insertEdge("C", "B", 5);
+  test.insertEdge("C", "E", 5);
+  test.insertEdge("B", "A", 7);
+  test.insertEdge("E", "D", 7);
 
   Vertex start = "A";
-  Vertex end = "C";
+  Vertex end = "E";
 
   //BFS bfs(test);
   //std::vector<Vertex> output0 = bfs.findShortestPath(start, end); 
@@ -44,16 +44,17 @@ int main() {
   //}
   //std::cout << std::endl;
 
-  Dijkstra dj(test);
-  std::vector<Vertex> output1 = dj.findShortestPath(start, end); 
+  Dijkstra dj(data.getGraph());
+  std::vector<Vertex> output1 = dj.findShortestPath("YUA", "JAM"); 
 
   for (Vertex vertex : output1) {
     std::cout << vertex << " ";
   }
   std::cout << std::endl;
 
-  std::vector<Vertex> output2 = findCentralAirports(1, test); 
+  std::vector<Vertex> output2 = findCentralAirports(1, data.getGraph()); 
 
+  std::cout<<"Central Vertices: "<<std::endl;
   for (Vertex vertex : output2) {
     std::cout << vertex << " ";
   }
