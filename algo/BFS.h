6f2a92b13dc2bcc452b88713f10352e15dc3fcbd
andrewdragontian <andrewdragontianCS@gmail.com>
2020-12-11 18:08:34 -0600

    commeted algos

--- algo/BFS.h
+++ algo/BFS.h
@@ -8,7 +8,11 @@
 
 class BFS {
   public:
+    // Constructor to input graph to calculate on
     BFS(Graph&);
+
+    // Outputs a vector of vertices that has the shortest path using BFS
+    // Inputs are starting and ending vertex and a vector of vertices to avoid
     std::vector<Vertex> findShortestPath(const Vertex, const Vertex, const std::vector<Vertex>);
   
   private:
